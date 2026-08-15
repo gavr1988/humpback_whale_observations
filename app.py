ffd6613fded0744417121df5ad79a3311b18c976
gavr1988 <gavrewt@gmail.com>
2026-08-15 11:15:08 +0100

    loading data

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import streamlit as st
+import pandas as pd
 
 #Making the browser tab and page layout
+
 st.set_page_config(
     page_title="Humpback Whale Analytics",
     page_icon="🐋",
     layout="wide"
 )
+
+#Loading the cleaned whale observation dataset. 
+# @st.cache_data tells streamlit to remember the loaded data
+# this prevents the reading of the csv each time the app reruns. 
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv("whale_data_cleaned.csv")
+    return df
+
 
 #Main Page Title
 st.title("North Pacific Humpback Whale Analytics")
